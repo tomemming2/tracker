06ff3356ec69aed3c3cbefb9eed0bd1cd41df6d6
Tom Emming <tom@tinux-it.nl>
2021-08-16 13:57:10 +0200

    rewrote the input form, added additional text and added a lot of CSS styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
-
 // Form 
 window.addEventListener("load", function() {
   document.getElementById('input-form').addEventListener("submit", function(e) {
     e.preventDefault(); // before the code
 
     // Get the required information
-    var height = document.getElementById("height").value; // Height
-    var weight = document.getElementById("weight").value; // Weight
-    var age = document.getElementById("age").value; // Age
+    var height = document.getElementById("heightOutput").value; // Height
+    var weight = document.getElementById("weightOutput").value; // Weight
+    var age = document.getElementById("ageOutput").value; // Age
 
     // Work through the form elements to define the selected gender and activity;
     var formElements = document.getElementById('input-form').elements;
@@ -18,16 +17,26 @@ window.addEventListener("load", function() {
     var BMI = CalcBMI(height, weight);
     if ( BMI < 18.5 ) {
         document.getElementById("bmi-value").innerHTML = (BMI + " (Underweight)");
-        document.getElementById("bmi-value").style.color = "orange";
+        document.getElementById("bmi-result-card").style.backgroundColor = "orange";
+        document.getElementById("bmi-result-text").innerHTML = "Your weight is way too low. This is not good for your health. Contact your doctor to determine the possible cause."
     } else if ( BMI > 18.5 && BMI < 25 ) {
     document.getElementById("bmi-value").innerHTML = (BMI + " (Healthy)");
-    document.getElementById("bmi-value").style.color = "green";
+    document.getElementById("bmi-result-card").style.backgroundColor = "green";
+    document.getElementById("bmi-result-text").innerHTML = "Your weight is healthy. Good! Continue to eat healthy and get enough exercise to keep it that way.\
+    The BMI does not say everything about your health. Your waist circumference is also important for optimal advice."
     } else if ( BMI > 25 && BMI < 30 ) {
         document.getElementById("bmi-value").innerHTML = (BMI + " (Overweight)");
-        document.getElementById("bmi-value").style.color = "orange";
+        document.getElementById("bmi-result-card").style.backgroundColor = "orange";
+        document.getElementById("bmi-result-text").innerHTML = "Your weight is too high. We recommend that you have your blood pressure,\
+         cholesterol and blood sugar measured by your doctor. This partly determines how important losing weight is for your health.\
+         Based on this, your doctor will give advice or refer you for help."
     } else if ( BMI > 30 ) {
         document.getElementById("bmi-value").innerHTML = (BMI + " (Obese)");
-        document.getElementById("bmi-value").style.color = "red";
+        document.getElementById("bmi-result-card").style.backgroundColor = "red";
+        document.getElementById("bmi-result-text").innerHTML = "Your weight is way too high. It is better for your health to lose weight.\
+        We strongly recommend that you have your blood pressure, cholesterol and blood sugar measured by your doctor. This partly determines how important losing weight is for your health.\
+        Based on this, the GP will refer you for help."
+
     }
 
     // Calculate BMR and display
